a1599a812dc7c9467db253ef93ee9986e469100e
Gregory Man <man.gregory@gmail.com>
2011-08-27 17:03:44 +0300

    removed 'spec/interop/test'

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,4 @@
 require File.dirname(__FILE__) + '/../lib/rvideo'
-
-# Because rspec's assertion style is a stupid, giant 
-# fucking waste of time. It's true!
-require 'spec/interop/test'
 require File.dirname(__FILE__) + "/support"
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
